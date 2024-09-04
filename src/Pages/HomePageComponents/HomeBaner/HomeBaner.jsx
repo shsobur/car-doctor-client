d5665fa4102c8d830df66f4ca9f1e9ea3cadb259
shsobur <shsoburhossen951@gmail.com>
2024-09-04 17:16:18 +0600

    Navbar and baner design completed

--- src/Pages/HomePageComponents/HomeBaner/HomeBaner.jsx
+++ src/Pages/HomePageComponents/HomeBaner/HomeBaner.jsx
@@ -1,10 +1,52 @@
 import "./HomeBaner.css";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Autoplay, Pagination } from "swiper/modules";
+
+import "swiper/css";
+import "swiper/css/pagination";
+
+import img1 from "../../../assets/images/banner/1.jpg";
+import img2 from "../../../assets/images/banner/2.jpg";
+import img3 from "../../../assets/images/banner/3.jpg";
+import img4 from "../../../assets/images/banner/4.jpg";
+import img5 from "../../../assets/images/banner/5.jpg";
+import img6 from "../../../assets/images/banner/6.jpg";
 
 const HomeBaner = () => {
   return (
-    <div>
-      <h2 className="text-2xl">Hello this is home page baner</h2>
-    </div>
+    <>
+      <div className="main_container">
+        <div className="main_baner_outer_container">
+          <div className="main_baner_inner_container">
+
+            <Swiper
+              loop={true}
+              autoplay={{
+                delay: 2500,
+                disableOnInteraction: false,
+              }}
+              pagination={{
+                dynamicBullets: true,
+              }}
+              modules={[Pagination, Autoplay]}
+              className="swiper_container mySwiper"
+            >
+              <SwiperSlide><img src={img1} /></SwiperSlide>
+              <SwiperSlide><img src={img2} /></SwiperSlide>
+              <SwiperSlide><img src={img3} /></SwiperSlide>
+              <SwiperSlide><img src={img4} /></SwiperSlide>
+              <SwiperSlide><img src={img5} /></SwiperSlide>
+              <SwiperSlide><img src={img6} /></SwiperSlide>
+            </Swiper>
+
+            <div className="baner_top_bg">
+              
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,10 +1,81 @@
 import "./Navbar.css";
+import logo from "../../assets/logo/logo.svg";
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <>
       <div className="main_container">
-        <h2>Hello navbar</h2>
+        <div className="main_nav_bar_outer_container">
+          <nav className="main_nav_bar_inner_container">
+            <div className="web_logo_container">
+              <img src={logo} alt="logo" />
+            </div>
+
+            <div id="main_navigation_container">
+              <ul>
+                <NavLink
+                  to="/"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-[#FF3811] text-lg font-bold underline"
+                      : "text-[#444444] text-lg font-bold"
+                  }
+                >
+                  <li>Home</li>
+                </NavLink>
+
+                <NavLink
+                  to="/about"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-[#FF3811] text-lg font-bold underline"
+                      : "text-[#444444] text-lg font-bold"
+                  }
+                >
+                  <li>About</li>
+                </NavLink>
+
+                <NavLink
+                  to="/service"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-[#FF3811] text-lg font-bold underline"
+                      : "text-[#444444] text-lg font-bold"
+                  }
+                >
+                  <li>Service</li>
+                </NavLink>
+
+                <NavLink
+                  to="/blog"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-[#FF3811] text-lg font-bold underline"
+                      : "text-[#444444] text-lg font-bold"
+                  }
+                >
+                  <li>Blog</li>
+                </NavLink>
+
+                <NavLink
+                  to="/contact"
+                  className={({ isActive }) =>
+                    isActive
+                      ? "text-[#FF3811] text-lg font-bold underline"
+                      : "text-[#444444] text-lg font-bold"
+                  }
+                >
+                  <li>Contact</li>
+                </NavLink>
+              </ul>
+            </div>
+
+            <div className="nav_bar_btn">
+              <button>Appointment</button>
+            </div>
+          </nav>
+        </div>
       </div>
     </>
   );
