a36fcec58125f03102387727ae34444c42ef723c
shsobur <shsoburhossen951@gmail.com>
2024-09-09 23:17:08 +0600

    Loade booking with user email

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -1,7 +1,23 @@
 import "./Booking.css";
 import img from "../../assets/images/checkout/checkout.png";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "../../components/AuthProvider/AuthProvider";
 
 const Booking = () => {
+  const {user} = useContext(AuthContext);
+  const [bookings, setBookings] = useState([]);
+  console.log(bookings);
+
+  const url = `http://localhost:5000/bookings?email=${user.email}`
+
+  useEffect(() => {
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setBookings(data);
+    })
+  }, [url])
+
   return (
     <>
       <div className="main_container">
@@ -13,7 +29,9 @@ const Booking = () => {
                 <img src={img}  />
               </div>
               <div className="booking_baner_overlay"></div>
-              <div className="booking_title"><h2>My Booking</h2></div>
+              <div className="booking_title">
+                <h2>My Booking</h2>
+              </div>
             </div>
 
           </div>
